2a5cb2a84d3d45879ea2f508bcbf8e01afaa6b45
michel <michel.cavaud@gmail.com>
2021-02-03 10:27:43 +0100

    First commit

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+
+define('DEBUG', true);
+define('BASE_URL', "localhost/nesti3");
+define('PATH_CTRL', "app/controller");
+define('PATH_MODEL', "app/model");  
+define('PATH_TOOLS', "app/tools");
+define('PATH_VIEW', "app/view");
+define('PATH_CONFIG', "app/config");
+
+define('PATH_CSS', "public/css");
+define('PATH_IMAGES', "public/images");
+define('PATH_JS', "public/js");
